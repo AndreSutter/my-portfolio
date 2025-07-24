5c807896c04682d0b3ff7820c298b077e71f8654
Andre Sutter <andre-sutter@hotmail.com>
2025-07-25 00:54:15 +0200

    add

--- src/app/projects.data.ts
+++ src/app/projects.data.ts
@@ -1,10 +1,12 @@
 export interface Project {
   slug: string;
   title: string;
-  description: string;       // now stores a translation key
-  implementation: string;    // now stores a translation key
+  description: string;      
+  implementation: string;   
   image: string;
   sticker?: string;
+  githubUrl?: string;        
+  liveUrl?: string;          
 }
 
 export const PROJECTS: Project[] = [
@@ -14,20 +16,26 @@ export const PROJECTS: Project[] = [
     description: 'project.join.description',
     implementation: 'project.join.implementation',
     image: 'assets/img/Laptop.png',
-    sticker: 'assets/img/Sticker.png'
+    sticker: 'assets/img/Sticker.png',
+    githubUrl: 'https://github.com/Getinger96/Join',
   },
   {
     slug: 'sharkie',
     title: 'Sharkie',
     description: 'project.sharkie.description',
     implementation: 'project.sharkie.implementation',
-    image: 'assets/img/sharkie.png'
+    image: 'assets/img/sharkie.png',
+    githubUrl: 'https://github.com/AndreSutter/sharkie',
+    liveUrl: 'https://andre-sutter.ch/sharkie/'
   },
   {
     slug: 'pokedex',
     title: 'Pokedex',
     description: 'project.pokedex.description',
     implementation: 'project.pokedex.implementation',
-    image: 'assets/img/pokedex.png'
+    image: 'assets/img/pokedex.png',
+    githubUrl: 'https://github.com/AndreSutter/pokedex', 
+    liveUrl: 'https://andre-sutter.ch/pokedex'
   }
 ];
+
